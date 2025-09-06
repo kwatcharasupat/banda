c96c461a70c41fab115cf1570ed6864af7ff604b
Karn Watcharasupat <kwatcharasupat@gatech.edu>
2025-09-06 02:27:53 +0000

    make slurm script

--- scripts/make_slurm_and_submit.py
+++ scripts/make_slurm_and_submit.py
@@ -1,2 +1,46 @@
 
 
+import os
+
+
+def make_slurm_and_submit(config_name: str, overrides: str | None = None, job_name: str | None = None):
+    
+    command = f"python scripts/train.py -cn {config_name}"
+    
+    if overrides is not None:
+        command += f" {overrides}"
+ 
+ 
+    slurm_template =  """
+    #!/bin/bash
+    #SBATCH -J{job_name}                    
+    #SBATCH -N1 --ntasks-per-node=1          
+    #SBATCH --cpu=16 --mem-per-cpu=16G       
+    #SBATCH --gres=gpu:V100:1 --mem-per-gpu=32G         
+    #SBATCH -t16h                            
+    #SBATCH -oReport-%j.out                  # Combined output and error messages file
+    #SBATCH --mail-type=BEGIN,END,FAIL       # Mail preferences
+    #SBATCH --mail-user=kwatchar3@gatech.edu # E-mail address for notifications
+
+    module load mamba
+    mamba activate banda
+    cd scratch/banda
+
+    srun {command}
+    """
+    
+    job_name = config_name.replace("/", "-").replace(".yaml", "") if job_name is None else job_name
+    slurm_script = slurm_template.format(job_name=job_name, command=command)
+    
+    script_path = f"slurm/{job_name}.sbatch"
+    if os.path.exists(script_path):
+        raise ValueError(f"Slurm script {script_path} already exists. Please remove it or choose a different job name.")
+    
+    with open(script_path, "w") as f:
+        f.write(slurm_script)
+        
+    os.system(f"sbatch {script_path}")
+
+if __name__ == "__main__":
+    import fire
+    fire.Fire(make_slurm_and_submit)
