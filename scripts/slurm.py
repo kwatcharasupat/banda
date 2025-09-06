0bca60c25fd610027daa7d41ee286563bfcffb36
Karn Watcharasupat <kwatcharasupat@gatech.edu>
2025-09-06 03:05:05 +0000

    update slurm out

--- scripts/slurm.py
+++ scripts/slurm.py
@@ -8,7 +8,7 @@ slurm_template =  """#!/bin/bash
 #SBATCH -J{job_name}                    
 #SBATCH -N1 --ntasks-per-node=1     
 #SBATCH --partition=ice-gpu
-#SBATCH --gres=gpu:A100:1              
+#SBATCH --gres=gpu:H100:1              
 #SBATCH --cpus-per-task=16 --mem-per-cpu=16G     
 #SBATCH --time=16:00:00                            
 #SBATCH --output=./slurm-out/Report-%j.out                  # Combined output and error messages file
