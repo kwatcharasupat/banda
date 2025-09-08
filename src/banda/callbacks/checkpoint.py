fb48ffb95a997e1592e51018d9774d34849dd63a
Karn Watcharasupat <kwatcharasupat@gatech.edu>
2025-09-08 12:05:38 -0400

    update

--- src/banda/callbacks/checkpoint.py
+++ src/banda/callbacks/checkpoint.py
@@ -3,12 +3,16 @@ from pytorch_lightning.callbacks import ModelCheckpoint
 
 
 class ModelCheckpointWithAutoRestart(ModelCheckpoint):
-    def __init__(self, config_name: str, **kwargs):
+    def __init__(self, config_name: str, should_trigger: bool, **kwargs):
         super().__init__(**kwargs)
         self.config_name = config_name
+        self.should_trigger = should_trigger
 
     def on_exception(self, trainer, pl_module, exception):
         super().on_exception(trainer, pl_module, exception)
+
+        if not self.should_trigger:
+            return
 
         last_checkpoint = self._last_checkpoint_saved
         command = f"python scripts/slurm.py make {self.config_name} --ckpt_path={last_checkpoint} --submit"
