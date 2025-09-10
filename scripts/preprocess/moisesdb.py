8249a0d0ba1c4adf1457fd1cdd1d6bdb0ebff872
Karn Watcharasupat <kwatcharasupat@gatech.edu>
2025-09-10 16:00:02 -0400

    add moises

--- scripts/preprocess/moisesdb.py
+++ scripts/preprocess/moisesdb.py
@@ -9,6 +9,7 @@ from pathlib import Path
 
 import warnings
 
+import torch
 import torchaudio as ta
 from torchaudio.functional import resample
 from torch.nn import functional as F
@@ -22,6 +23,8 @@ from typing import List
 from pydantic import BaseModel
 
 import structlog
+
+import librosa
 
 logger = structlog.get_logger()
 
@@ -256,6 +259,84 @@ def process_track_coarse(path: Path, config: DictConfig):
     np.savez(output_path, **{k: v.cpu().numpy() for k, v in data.items()}, fs=config.fs)
 
 
+def process_track_active(path: Path, config: DictConfig):
+
+    track_name = path.stem
+
+    splits = pd.read_csv(
+        Path(os.getenv("DATA_ROOT"), config.datasource_id, "splits.csv").expanduser()
+    ).set_index("song_id")
+    fold = splits.loc[track_name, "split"]
+
+    if fold == 4:
+        split = "val"
+    elif fold == 5:
+        split = "test"
+    else:
+        split = "train"
+
+    data_json_path = Path(path, "data.json")
+
+    with open(data_json_path, "r") as f:
+        data_json = json.load(f)
+        song_metadata = SongMetadata.model_validate(data_json)
+
+    for stem in song_metadata.stems:
+        stem_name = stem.stemName
+        for track in stem.tracks:
+            track_path = Path(path, stem_name, f"{track.id}.{track.extension}")
+
+            with warnings.catch_warnings():
+                warnings.simplefilter("ignore")
+                x, original_fs = ta.load(track_path)
+            if original_fs != config.fs:
+                x = resample(x, original_fs, config.fs)
+
+            process_stem_active(x, 
+                                stem_coarse=stem_name,
+                                stem_fine=track.trackType,
+                                track_id=track.id,
+                                config=config,
+                                split=split,
+            )
+
+
+def process_stem_active(x : torch.Tensor, *, 
+                        stem_coarse: str, stem_fine: str, track_id: str, config: DictConfig, split: str):
+
+    intervals: np.ndarray = librosa.effects.split(
+        x.numpy(),
+        top_db=60,
+        ref=0.0,
+        frame_length=config.frame_length_seconds * config.fs,
+        hop_length=config.hop_length_seconds * config.fs,
+    )
+
+    clips = []
+    for start, end in intervals:
+        clip = x[:, start:end]
+
+        clip_length_samples = int(config.clip_length_seconds * config.fs)
+        print(clip.shape, clip_length_samples)
+
+
+
+
+    output_path = Path(
+        os.getenv("DATA_ROOT"),
+        config.datasource_id,
+        "intermediates",
+        "npz-active",
+        str(split),
+        f"{track_name}.npz",
+    )
+    output_path.parent.mkdir(parents=True, exist_ok=True)
+
+    # np.savez(output_path, **{k: v.cpu().numpy() for k, v in data.items()}, fs=config.fs)
+
+
+
+
 @hydra.main(config_path="../../configs/preprocess")
 def main(config: DictConfig):
     tracks = list(
@@ -271,6 +352,9 @@ def main(config: DictConfig):
         process_map(process_track_fine, tracks, [config] * len(tracks), max_workers=16)
     elif config.mode == "raw":
         process_map(process_track_raw, tracks, [config] * len(tracks), max_workers=16)
+    elif config.mode == "active":
+        # process_map(process_track_active, tracks, [config] * len(tracks), max_workers=16)
+        process_track_active(tracks[0], config)
     else:
         raise NotImplementedError(f"Mode {config.mode} not implemented.")
 
